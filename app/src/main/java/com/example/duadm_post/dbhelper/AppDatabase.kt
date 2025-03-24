f9e7ced3fd7b94723fe6b4f5e6989957666d8a4f
rahuldhanawade <60505899+rahuldhanawade@users.noreply.github.com>
2025-03-24 11:28:06 +0530

    AppDatabase changes

--- app/src/main/java/com/example/duadm_post/dbhelper/AppDatabase.kt
+++ app/src/main/java/com/example/duadm_post/dbhelper/AppDatabase.kt
@@ -14,20 +14,4 @@ abstract class AppDatabase : RoomDatabase() {
 
     abstract fun postDao(): PostDao
 
-    companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getDatabase(@ApplicationContext context: Context): AppDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "post_database"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
